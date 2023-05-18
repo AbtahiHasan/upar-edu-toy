f082a5ec5b7159d3976423e5b714b34ec851c502
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-19 00:12:47 +0600

    search feature done

--- src/pages/AllToys.jsx
+++ src/pages/AllToys.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import TobularFormToyCard from "../components/TobularFormToyCard";
+import { FiSearch } from "react-icons/fi";
 
 const AllToys = () => {
     const [toys, setToys] = useState([])
@@ -9,9 +10,19 @@ const AllToys = () => {
         .then(res => res.json())
         .then(data => setToys(data))
     }, [])
-  
+    const search = (e) => {
+        e.preventDefault()
+        
+        fetch(`https://upar-edu-toy.vercel.app/search?query=${e.target.search_input.value}`)
+        .then(res => res.json())
+        .then(data => setToys(data))
+    }
     return (
         <main className="container">
+            <form onSubmit={search} className="overflow-hidden relative  my-10 h-[45px] max-w-[500px] min-w-[280px] rounded-full mx-auto">
+                    <input name="search_input" className="h-full rounded-full border outline-none w-full px-5" type="text" autoComplete="off" placeholder="Search...." />
+                    <button className="bg-[#00abe4] w-[55px] flex justify-between items-center text-center absolute right-0 top-0 h-full px-3 text-white" type="submit"><FiSearch/></button>
+            </form>
             <div className='mt-[50px] overflow-x-auto'>
                 <table className='my-10 min-w-[960px] w-full mx-auto'>
                     <thead className="bg-slate-200">
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,7 +5,27 @@ const Blog = () => {
     useTitle("blog")
     return (
         <main>
-            blog
+            <div className='container'>
+                <h1 className='text-3xl font-bold text-center my-5'>Question's <span className='text-[#B2A4FF]'>Answer....</span></h1>
+                <div className='grid md:grid-cols-2 gap-4 mt-10 mb-20'>
+                    <div className='p-3 border-2 rounded-md w-full h-[100%] border-[#00abe4]'>
+                        <h1 className='text-xl py-3'>What is an access token and refresh token? How do they work and where should we store them on the client-side?</h1>
+                        <p> </p>
+                    </div>
+                    <div className='p-3 border-2 rounded-md border-[#00abe4]'>
+                        <h1 className='text-xl py-3'>Compare SQL and NoSQL databases?</h1>
+                        <p></p>
+                    </div>
+                    <div className='p-3 border-2 rounded-md border-[#00abe4]'>
+                        <h1 className='text-xl py-3'>What is express js? What is Nest JS ?</h1>
+                        <p></p>
+                    </div>
+                    <div className='p-3 border-2 rounded-md border-[#00abe4]'>
+                        <h1 className='text-xl py-3'>What is MongoDB aggregate and how does it work ?</h1>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 };
