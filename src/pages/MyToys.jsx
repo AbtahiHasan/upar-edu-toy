479258edda4312ff5f4243c639050138d4c2f27f
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-18 20:10:13 +0600

    get Data by email done

--- src/pages/MyToys.jsx
+++ src/pages/MyToys.jsx
@@ -1,10 +1,43 @@
-
+import { useEffect, useState } from "react";
+import TobularFormToyCard from "../components/TobularFormToyCard";
+import { useAuth } from "../context/AuthProvider";
 
 const MyToys = () => {
+    const [toys, setToys] = useState([])
+    const {user} = useAuth()
+
+
+    const url = `http://localhost:5000/seller?email=${user?.email}`;
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [url]);
+    const deleteToy = () => {
+        console.log("delete")
+    }
     return (
-        <div>
-            
-        </div>
+        <main className="container">
+            <div className='mt-[50px] overflow-x-auto'>
+                <table className='my-10 min-w-[960px] w-full mx-auto'>
+                    <thead className="bg-slate-200">
+                        <tr>
+                            <th className='p-4'>Seller</th>
+                            <th className='p-4'>Toy Name</th>
+                            <th className='p-4'>Sub-category</th>
+                            <th className='p-4'>Price</th>
+                            <th className='p-4'>Avilable Quantity</th>
+                            <th className='p-4'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            toys.map(toy => <TobularFormToyCard key={toy._id} toy={toy} deleteToy={deleteToy}/>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </main>
     );
 };
 
