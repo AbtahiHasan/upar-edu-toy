9f536850e5a4efdf00cc285461ebe458cd367c26
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-18 16:41:37 +0600

    all toys page responsive update

--- src/pages/AllToys.jsx
+++ src/pages/AllToys.jsx
@@ -8,25 +8,27 @@ const AllToys = () => {
         console.log("delete")
     }
     return (
-        <div className='mt-[50px] overflow-x-auto'>
-            <table className='my-10 min-w-[960px] mx-auto'>
-                <thead className="bg-slate-200">
-                    <tr>
-                        <th className='p-4'>Seller</th>
-                        <th className='p-4'>Toy Name</th>
-                        <th className='p-4'>Sub-category</th>
-                        <th className='p-4'>Price</th>
-                        <th className='p-4'>Avilable Quantity</th>
-                        <th className='p-4'>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        toys.map(toy => <TobularFormToyCard key={toy._id} toy={toy} deleteToy={deleteToy}/>)
-                    }
-                </tbody>
-            </table>
-        </div>
+        <main className="container">
+            <div className='mt-[50px] overflow-x-auto'>
+                <table className='my-10 min-w-[960px] w-full mx-auto'>
+                    <thead className="bg-slate-200">
+                        <tr>
+                            <th className='p-4'>Seller</th>
+                            <th className='p-4'>Toy Name</th>
+                            <th className='p-4'>Sub-category</th>
+                            <th className='p-4'>Price</th>
+                            <th className='p-4'>Avilable Quantity</th>
+                            <th className='p-4'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            toys.map(toy => <TobularFormToyCard key={toy._id} toy={toy} deleteToy={deleteToy}/>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </main>
     );
 };
 
