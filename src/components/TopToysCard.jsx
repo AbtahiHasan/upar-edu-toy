587f143e3cb409df936b187094b931979e5c67d2
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-20 18:38:53 +0600

    top ratedview btn message done

--- src/components/TopToysCard.jsx
+++ src/components/TopToysCard.jsx
@@ -1,10 +1,19 @@
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import Rating from "react-rating";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import { toast } from "react-toastify";
 
 
 const TopToysCard = ({toy}) => {
     const {_id, name,photo_url, rating, price} = toy
+    const {user} = useAuth
+    const notLoginMassage = () => {
+      if(!user) {
+        toast("You have to log in first to view details")
+      }
+  }
+
     return (
         <article data-aos="fade-up" className='grid grid-cols-3 gap-3 p-5 border rounded-md '>         
                 <figure><img className="h-52" src={photo_url} alt="toys image"/></figure>
@@ -22,7 +31,7 @@ const TopToysCard = ({toy}) => {
                 </p>
                   <div className="absolute bottom-2 left-0">
                     
-                        <Link to={`/toy/${_id}`}><button className="active px-6 rounded-md">View Details</button></Link>                        
+                        <Link onClick={notLoginMassage} to={`/toy/${_id}`}><button className="active px-6 rounded-md">View Details</button></Link>                        
                     
                   </div>
                 </div>
