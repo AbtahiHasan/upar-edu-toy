7788c3d243956d2202c42ced012503ed3913e522
AbtahiHasan <anikidpersonal@gmail.com>
2023-06-18 15:04:13 +0600

    age by responsive done

--- src/components/AgeBy.jsx
+++ src/components/AgeBy.jsx
@@ -3,6 +3,13 @@ import image2 from "/children/image-2.jpg"
 import image3 from "/children/image-3.jpg"
 import image4 from "/children/image-4.jpg"
 import image5 from "/children/image-5.jpg"
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination } from "swiper";
 
 const AgeBy = () => {
     const childrens = [
@@ -32,18 +39,38 @@ const AgeBy = () => {
     return (
         <main className="mt-[140px] border-t-4 border-[#00abe4] p-5 relative">
                 <h2 className='whitespace-nowrap text-center font-bold text-2xl mb-10 absolute -top-5 transform left-1/2 -translate-x-1/2'><span className='bg-[#00abe4] text-white py-3 px-2'>By</span><span className='border-2 bg-white border-[#00abe4] text-[#00abe4] py-3 px-2'>Age</span></h2>
-                <section className="w-[1240px] overflow-x-auto grid grid-cols-5 gap-3 mt-10">
-                    {
-                        childrens.map((children, i) => {
-                            return (
-                                <article data-aos="fade-up" className="p-5 rounded border" key={i}>
-                                    <img className="h-[200px] rounded" src={children.image} alt="child image" />
-                                    <h2 className="text-xl font-bold text-center mt-5">{children.name}</h2>
-                                </article>
-                            )
-                        })
-                    }
-                </section>
+
+            
+                    <Swiper
+                          effect={"coverflow"}
+                         grabCursor={true}
+                         centeredSlides={true}
+                         slidesPerView={"3"}
+                         coverflowEffect={{
+                           rotate: 50,
+                           stretch: 0,
+                           depth: 100,
+                           modifier: 1,
+                           slideShadows: true,
+                         }}
+                         pagination={true}
+                         modules={[EffectCoverflow, Pagination]}
+                         className="mt-10"
+                            >
+                        {
+                            childrens.map((children, i) => {
+                                return (
+                                    <SwiperSlide key={i}>
+                                    <article data-aos="fade-up" className="p-5 rounded border" >
+                                        <img className="mx-auto h-[200px] rounded" src={children.image} alt="child image" />
+                                        <h2 className="text-xl font-bold text-center mt-5">{children.name}</h2>
+                                    </article>
+                                    </SwiperSlide>
+                                )
+                            })
+                        }
+                         </Swiper>
+
         </main>
     );
 };
