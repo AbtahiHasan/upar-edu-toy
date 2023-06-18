651b4e70b7feacdfe6c9e8ab22552ff2b69745ac
AbtahiHasan <anikidpersonal@gmail.com>
2023-06-18 14:32:56 +0600

    home page done

--- src/components/TopToysCard.jsx
+++ src/components/TopToysCard.jsx
@@ -3,7 +3,7 @@ import Rating from "react-rating";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
 import { toast } from "react-toastify";
-
+import {motion} from "framer-motion"
 
 const TopToysCard = ({toy}) => {
     const {_id, name,photo_url, rating, price} = toy
@@ -15,12 +15,15 @@ const TopToysCard = ({toy}) => {
   }
 
     return (
-        <article style={{background: `url('${photo_url}')`, backgroundSize: "cover"}} data-aos="fade-up" className='grid grid-cols-3 gap-3 p-5 border rounded-md '>         
-              
-                <div className="col-span-2 relative">
-                  <h2 className="text-xl font-bold">{name}</h2>
+      <Link  onClick={notLoginMassage} to={`/toy/${_id}`}>
+        <article  data-aos="fade-up" className=" overflow-hidden   p-5 border rounded-md h-[250px] w-full">         
+         <div className="before:content-[''] w-full h-full relative before:absolute before:bg-[#000000b7] before:top-0 before:right-0 before:left-0 before:bottom-0">
+            <motion.img whileHover={{scale: 1.1}} className="w-full h-full  object-cover" src={photo_url} alt="" />
+        </div>
+            <div className="absolute top-1/2 transform p-3 -translate-y-1/2 z-10 text-white">
+                  <div className="relative w-full h-full"> <h2 className="text-lg font-bold">{name}</h2>
                   <p>Price: ${price}</p>
-                  <p className="my-3 flex items-center gap-3">
+                  <p className="my-6 flex items-center gap-3">
                 <Rating
                      placeholderRating={rating}
                      emptySymbol={<AiOutlineStar className="text-[#ff9529]"/>}
@@ -29,13 +32,17 @@ const TopToysCard = ({toy}) => {
                      readonly
                 /> <span>{rating}</span>
                 </p>
-                  <div className="absolute bottom-2 left-0">
-                    
-                        <Link onClick={notLoginMassage} to={`/toy/${_id}`}><button className="active px-6 rounded-md">View Details</button></Link>                        
-                    
-                  </div>
+                
+     
+
+                      
+
+
+                      </div>
                 </div>
-        </article>
+
+          </article>
+          </Link> 
     );
 };
 
