fedad0909095ec801e77d139cabf1a6ee9f76b74
AbtahiHasan <anikidpersonal@gmail.com>
2023-06-18 11:33:54 +0600

    count ui changed

--- src/components/CountUp.jsx
+++ src/components/CountUp.jsx
@@ -3,9 +3,9 @@ import CountUp from 'react-countup';
 const Count = () => {
     return (
         <section className="bg-[#00abe4] mt-[140px] grid md:grid-cols-3 gap-5 px-10">
-            <div className='flex justify-center items-center h-[100px] flex-col text-2xl font-bold text-white'>
+            <div className='flex justify-center items-center h-[200px] flex-col text-2xl font-bold text-white'>
                 <h2>Products</h2>
-                <CountUp start={0} end={250} delay={0}>
+                <CountUp start={0} end={250} delay={2}>
                 {({ countUpRef }) => (
                   <div >
                     <span ref={countUpRef} />
@@ -13,9 +13,9 @@ const Count = () => {
                 )}
                 </CountUp>
             </div>
-            <div className='flex justify-center items-center h-[100px] flex-col text-2xl font-bold text-white'>
+            <div className='flex justify-center items-center h-[200px] flex-col text-2xl font-bold text-white'>
                 <h2>Customers</h2>
-                <CountUp start={0} end={12000} delay={0}>
+                <CountUp start={0} end={12000} delay={2}>
                 {({ countUpRef }) => (
                   <div >
                     <span ref={countUpRef} />
@@ -23,9 +23,9 @@ const Count = () => {
                 )}
                 </CountUp>
             </div>
-            <div className='flex justify-center items-center h-[100px] flex-col text-2xl font-bold text-white'>
+            <div className='flex justify-center items-center h-[200px] flex-col text-2xl font-bold text-white'>
                 <h2>Happy</h2>
-                <CountUp start={0} end={3000} delay={0}>
+                <CountUp start={0} end={3000} delay={2}>
                   {({ countUpRef }) => (
                     <div >
                       <span ref={countUpRef} />
